02990fcccf3544dd35cdb515ad4599fdc141dab2
william gerardin <w.gerardin@outlook.fr>
2023-04-07 11:59:28 +0200

    montaf

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice binom de William et Florian</title>
+    <meta name="description" content="Exercice vendredi matin">
+    <link rel="stylesheet" href="./assets/style/css/style.css">
+</head>
+
+<body>
+
+
+    <header>
+
+        <h1>la pêche</h1>
+
+        <nav>
+            <ul>
+                <li>Accueil</li>
+                <li>Contact</li>
+                <li>Techniques</li>
+                <li>Matériel</li>
+            </ul>
+        </nav>
+    </header>
+
+
+    <main>
+
+        <article>
+
+
+            <h2>Pêche au coup</h2>
+            <div class="coup">
+                <img class="img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrBoSR5a4wkJl66htcXkHAiiFW8x-Hq80Pe-5gLdpy&s" alt="Un dessin d'un pêcheur qui pêche à la pêche au coup">
+                <p>La pêche au coup à la petite canne est la technique de pêche la plus populaire, celle que pratiquent un grand nombre de pêcheurs, en particulier les débutants. Elle porte son nom du fait que le pêcheur prépare son "coup", c'est à dire la zone qu'il souhaite pêcher grâce à des boules d'amorces.</p>
+            </div>
+        </article>
+        <div>
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+        <video src=""></video>
+
+        <article>
+
+            <h2>Pêche à l'anglaise</h2>
+            <div class="anglaise">
+                <img class="img" src="https://c8.alamy.com/compfr/2dahbm4/illustration-vecteur-dessiner-a-la-main-des-gribouts-de-pecheur-tenant-le-crochet-de-poisson-isole-sur-fond-blanc-2dahbm4.jpg" alt="Une photo de la pêche à l'anglaise">
+                <p>La pêche à l'anglaise est une adaptation de la pêche au coup. Elle se pratique avec un moulinet et une canne à anneaux à emmanchement ou télescopique. D'une longueur de 12' (3,60 m) à 15' (4,50 m), elle est munie de nombreux anneaux surélevés qui favorisent la glisse du nylon et évite que ce dernier colle à la canne.</p>
+            </div>
+
+        </article>
+        <div>
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+        <video src=""></video>
+
+        <article>
+
+            <h2>Pêche au carnassier</h2>
+            <div class="carna">
+                <img class="img" src="https://i.pinimg.com/236x/d0/30/53/d03053280988409d9954eab1efb470f9.jpg" alt="Une image sur la pêche au carnassier">
+                <p>La pêche au carnassier est une technique de pêche qui vise à attraper des poissons prédateurs tels que le brochet, le sandre et le black-bass.</p>
+            </div>
+            </div>
+        </article>
+        <div>
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+        <video src=""></video>
+
+        <article>
+
+            <h2>Pêche à la carpe</h2>
+            <div class="carpe">
+                <img class="img" src="https://cdn.pixabay.com/photo/2019/03/13/00/09/carp-4052148_960_720.jpg" alt="Une photo de la pêche à la carpe">
+                <p>La pêche de la carpe se pratique de façon générale avec des cannes allant de 3m (10ft/pieds) à 3,90m (13ft) et une puissance entre 2,5lbs et 3,5lbs en fonction des conditions : petits étangs et petites carpes ne nécessitent pas le même matériel que grands lacs et grandes rivières peuplés de très grosses carpes.</p>
+            </div>
+
+        </article>
+        <div>
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+        <video src=""></video>
+
+
+
+
+
+
+    </main>
+
+
+
+
+
+
+
+</body>
+
+</html>
